1341dbc766c86ddbbc083ae0ac2146c4010a91c3
Joshua L. Adelman <joshua.adelman@gmail.com>
2021-12-03 14:23:03 -0500

    complete aoc03

---
+++ rust/aoc03/src/main.rs
@@ -0,0 +1,98 @@
+use anyhow::Result;
+
+pub fn main() -> Result<()> {
+    let start = std::time::Instant::now();
+    let soln_a = solve_a()?;
+    eprintln!("Part A elapsed {:?}", start.elapsed());
+    println!("solution part A: {}", soln_a);
+
+    let start = std::time::Instant::now();
+    let soln_b = solve_b()?;
+    eprintln!("Part B elapsed {:?}", start.elapsed());
+    println!("solution part B: {}", soln_b);
+
+    Ok(())
+}
+
+fn flipnum(mut n: u64) -> u64 {
+    let mut x = 1;
+    while x <= n {
+        n ^= x;
+        x <<= 1;
+    }
+
+    n
+}
+
+pub fn solve_a() -> Result<u64> {
+    let input = include_str!("../input");
+    let width = input.lines().next().unwrap().len();
+
+    let dec = input
+        .lines()
+        .map(|line| usize::from_str_radix(line, 2))
+        .collect::<Result<Vec<_>, _>>()?;
+
+    let nnums = dec.len();
+
+    let gamma = dec
+        .iter()
+        .fold(vec![0; width], |mut acc, x| {
+            for (i, e) in acc.iter_mut().enumerate().take(width) {
+                if x >> i & 1 == 0 {
+                    *e += 1;
+                }
+            }
+            acc
+        })
+        .iter()
+        .enumerate()
+        .map(|(i, x)| ((*x < nnums / 2) as u64) << i)
+        .sum::<u64>();
+
+    let epsilon = flipnum(gamma);
+
+    Ok(gamma * epsilon)
+}
+
+pub fn solve_b() -> Result<u64> {
+    let input = include_str!("../input");
+    let width = input.lines().next().unwrap().len();
+
+    let dec = input
+        .lines()
+        .map(|line| u64::from_str_radix(line, 2))
+        .collect::<Result<Vec<_>, _>>()?;
+
+    let oxygen = (0..width)
+        .rev()
+        .scan(dec.clone(), |candidates, i| {
+            let nnums = candidates.len();
+
+            let ones_cnt = candidates.iter().filter(|&x| x >> i & 1 == 1).count();
+            let target = if ones_cnt >= (nnums - ones_cnt) { 1 } else { 0 };
+
+            candidates.retain(|&x| x >> i & 1 == target);
+
+            candidates.first().copied()
+        })
+        .last()
+        .unwrap();
+
+    let co2 = (0..width)
+        .rev()
+        .scan(dec, |candidates, i| {
+            let nnums = candidates.len();
+
+            let ones_cnt = candidates.iter().filter(|&x| x >> i & 1 == 1).count();
+            let target = if ones_cnt >= (nnums - ones_cnt) { 0 } else { 1 };
+
+            candidates.retain(|&x| x >> i & 1 == target);
+
+            candidates.first().copied()
+        })
+        .last()
+        .unwrap();
+
+    Ok(oxygen * co2)
+}
--- rust/benchmark/src/lib.rs
+++ rust/benchmark/src/lib.rs
@@ -28,5 +28,7 @@ pub fn solvers() -> &'static [Soln] {
         soln!(aoc01::solve_b, "aoc01b"),
         soln!(aoc02::solve_a, "aoc02a"),
         soln!(aoc02::solve_b, "aoc02b"),
+        soln!(aoc03::solve_a, "aoc03a"),
+        soln!(aoc03::solve_b, "aoc03b"),
     ]
 }
